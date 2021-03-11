7b5e28c45b0c4dc3792d5c11730c869e327ee922
karolk98 <47919588+karolk98@users.noreply.github.com>
2021-03-11 02:53:49 +0100

    Learning using batch

--- Perceptron.py
+++ Perceptron.py
@@ -33,14 +33,13 @@ class Perceptron:
         Classification = 1
         Regression = 2
 
-    def __init__(self, problem_type=ProblemType.Classification, classes=None, hidden_layers=2, hidden_layer_size=None,
+    def __init__(self, problem_type=ProblemType.Classification, classes=None, hidden_layers=[2, 2],
                  activation=sigmoid, dActivation=dSigmoid, loss=quad, dLoss=dQuad, final=sigmoid, dFinal=dSigmoid,
                  bias=False,
-                 batch_size=100, iterations=20, learning_rate=0.5, momentum=0.5):
+                 batch_size=1000, epochs=10, learning_rate=0.5, momentum=0.5):
         self.problem_type = problem_type
         self.classes = classes if problem_type == self.ProblemType.Classification else 1
         self.hidden_layers = hidden_layers
-        self.hidden_layer_size = hidden_layer_size
         self.activation = activation
         self.dActivation = dActivation
         self.loss = loss
@@ -49,21 +48,18 @@ class Perceptron:
         self.dFinal = dFinal
         self.bias = bias
         self.batch_size = batch_size
-        self.iterations = iterations
+        self.epochs = epochs
         self.learning_rate = learning_rate
         self.momentum = momentum
 
     def load(self, path):
         self.traindata = pd.read_csv(path)
-        print(self.traindata[0:10])
 
     def initialize(self):
         self.classes = self.classes if self.classes is not None else len(self.traindata['cls'].unique())
         self.data_dim = len(self.traindata.columns) - 1
-        self.hidden_layer_size = self.hidden_layer_size if self.hidden_layer_size is not None else int(
-            (self.data_dim + self.classes) / 2)
         layer_sizes = [self.data_dim]
-        layer_sizes.extend([self.hidden_layer_size for x in range(self.hidden_layers)])
+        layer_sizes.extend(self.hidden_layers)
         layer_sizes.append(self.classes)
         self.layers = []
         self.biases = []
@@ -79,38 +75,68 @@ class Perceptron:
         samples = self.traindata.iloc[:, 0:-1].values
         classes = self.traindata.iloc[:, -1].values
 
-        choice = range(len(samples))
-        for a in range(1000):
-            index = np.random.choice(choice)
-            y = [samples[index]]
-            desired_out = np.array([(x == classes[index]) * 1 for x in range(1, self.classes + 1)])
+        gradients = []
+        for epoch in range(0, self.epochs):
+            batch_start = 0
+            while batch_start < len(samples):
+                batch_end = min(batch_start+self.batch_size, len(samples))
 
-            for idx, layer in enumerate(self.layers):
-                output = np.matmul(layer, y[idx].T)
-                if idx == len(self.layers)-1:
-                    output = self.final(output)
+                batch_size = batch_end-batch_start
+                new_gradients = []
+
+                for m in range(batch_start, batch_end):
+                    y, activated = self.forward(samples[m])
+                    desired_out = np.zeros(self.classes)
+                    desired_out[classes[m]-1] = 1
+                    mth_gradients = self.gradient(y, activated, desired_out)
+                    if m == batch_start:
+                        new_gradients = mth_gradients
+                    else:
+                        new_gradients = [np.add(new_gradients[idx], mth_gradients[idx]) for idx in range(len(new_gradients))]
+
+                new_gradients = [gradient/batch_size for gradient in new_gradients]
+                if len(gradients)==0:
+                    gradients = new_gradients
                 else:
-                    output = self.activation(output)
-                y.append(output)
+                    gradients = [np.add(gradients[idx]*self.momentum, new_gradients[idx]*(1-self.momentum)) for idx in range(len(new_gradients))]
 
-            loss = self.loss(y[-1], desired_out)
+                self.apply_gradient(gradients)
 
-            dLoss = self.dLoss(y[-1], desired_out)
-            gradients = []
-            ygradient = np.multiply(dLoss, self.dFinal(y[-1]))
-            gradient = np.outer(ygradient, y[-2])
+                batch_start = batch_end
+
+    def forward(self, batch):
+        y = [batch]
+        output = batch
+        activated = [batch]
+        for idx, layer in enumerate(self.layers):
+            output = np.matmul(layer, output)
+            y.append(output)
+            if idx == len(self.layers) - 1:
+                output = self.final(output)
+            else:
+                output = self.activation(output)
+            activated.append(output)
+        return y, activated
+
+    def gradient(self, ys, activated, desired):
+        dLoss = self.dLoss(activated[-1], desired)
+        gradients = []
+        ygradient = np.multiply(dLoss, self.dFinal(ys[-1]))
+        gradient = np.outer(ygradient, activated[-2])
+        gradients.append(gradient)
+
+        for i in range(len(self.layers) - 2, -1, -1):
+            next_layer = self.layers[i + 1]
+            ygradient = np.multiply(np.matmul(next_layer.T, ygradient), self.dActivation(ys[i + 1]))
+            gradient = np.outer(ygradient, activated[i])
             gradients.append(gradient)
 
-            for i in range(len(self.layers)-2, -1, -1):
-                next_layer = self.layers[i+1]
-                ygradient = np.multiply(np.matmul(next_layer.T, ygradient), self.dActivation(y[i+1]))
-                gradient = np.outer(ygradient, y[i])
-                gradients.append(gradient)
+        gradients = gradients[::-1]
+        return gradients
 
-            gradients = gradients[::-1]
-
-            for i in range(len(self.layers)-1, -1, -1):
-                self.layers[i] = np.subtract(self.layers[i], gradients[i])
+    def apply_gradient(self, gradients):
+        for i in range(len(self.layers)):
+            self.layers[i] = np.subtract(self.layers[i], self.learning_rate*gradients[i])
 
     def test(self, path):
         testdata = pd.read_csv(path)
@@ -134,7 +160,7 @@ class Perceptron:
         print(counter/len(samples))
 
 
-perceptron = Perceptron(hidden_layers=2)
-perceptron.load(r"C:\Users\Karol\Downloads\project-1-part-1-data\project-1-part-1-data\data.three_gauss.train.10000.csv")
+perceptron = Perceptron(hidden_layers=[2, 2], batch_size=1, epochs=1)
+perceptron.load(r"C:\Users\Karol\Downloads\project-1-part-1-data\project-1-part-1-data\data.simple.train.10000.csv")
 perceptron.train()
-perceptron.test(r"C:\Users\Karol\Downloads\project-1-part-1-data\project-1-part-1-data\data.three_gauss.test.10000.csv")
+perceptron.test(r"C:\Users\Karol\Downloads\project-1-part-1-data\project-1-part-1-data\data.simple.test.10000.csv")
